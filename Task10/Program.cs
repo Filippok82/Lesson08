fda258bc1a2d66c4c11e1a265be9cb86b6de20fa
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-12-16 19:54:02 +0300

    Lesson08 Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,85 @@
+﻿// 10.Дан целочисленный двумерный массив, размерности n х m. 
+//Выяснить, в какой строке последовательность является возрастающей или убывающей.
+void SetArray2D(int[,] a)
+
+{
+
+    for (int i = 0; i < a.GetLength(0); i++)
+
+    {
+
+        for (int j = 0; j < a.GetLength(1); j++)
+
+        {
+
+            a[i, j] = new Random().Next(0, 40);
+
+        }
+
+    }
+
+}
+
+void Print(int[,] a)
+
+{
+
+    for (int i = 0; i < a.GetLength(0); i++)
+
+    {
+
+        for (int j = 0; j < a.GetLength(1); j++)
+
+        {
+
+            Console.Write($"{a[i, j],4}");
+
+        }
+
+        Console.WriteLine();
+
+    }
+
+}
+
+
+void FirstElementSequence(int[,] a)
+{
+    
+    
+     for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            if (a[i, j] > a[i,j+1])
+            {
+                Console.WriteLine($"{a[i, j]} последовательность убывающая ");
+                break;
+            }
+            else if (a[i, j] < a[i,j+1])
+            {
+                Console.WriteLine($"{a[i, j]}  последовательность возрастающая ");
+                break;
+            }
+
+        }
+
+    }
+
+    
+
+    
+}
+
+
+
+int[,] a = new int [3, 4];
+SetArray2D(a);
+Print(a);
+FirstElementSequence(a);
+
+
+
+
+
+
