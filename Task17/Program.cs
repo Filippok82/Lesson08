01ab15183ef64b5f52a384fdfed9f669aee90e9b
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-12-28 13:25:24 +0300

    Lesson08 Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,73 @@
+﻿//17. Дан целочисленный двумерный массив, размерности n х m. Заменить все элементы на их противоположные значения.
+
+void SetArray2D(int[,] a)
+
+{
+
+    for (int i = 0; i < a.GetLength(0); i++)
+
+    {
+
+        for (int j = 0; j < a.GetLength(1); j++)
+
+        {
+
+            a[i, j] = new Random().Next(0, 40);
+
+        }
+
+    }
+
+}
+
+void Print(int[,] a)
+
+{
+
+    for (int i = 0; i < a.GetLength(0); i++)
+
+    {
+
+        for (int j = 0; j < a.GetLength(1); j++)
+
+        {
+
+            Console.Write($"{a[i, j],4}");
+
+        }
+
+        Console.WriteLine();
+
+    }
+
+}
+
+
+void OppositeElement(int[,] a)
+{
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+
+            a[i, j] = a[i, j] * -1;
+            Console.Write($"{a[i, j],4}");
+
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+int[,] a = new int[3, 3];
+SetArray2D(a);
+Print(a);
+Console.WriteLine();
+OppositeElement(a);
+
+
+
+
+
