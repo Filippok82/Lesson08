24d5b6ba112effe9bdb64c73547d277c3936c2ca
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-01-06 16:43:47 +0300

    Lesson08 Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,81 @@
+﻿// Дан целочисленный двумерный массив, размерности n х m. 
+//Вывести на экран те элементы, у которых остаток от деления на m равен k. 
+
+void SetArray2D(int[,] a)
+
+{
+    Random random = new Random();
+    for (int i = 0; i < a.GetLength(0); i++)
+
+    {
+
+        for (int j = 0; j < a.GetLength(1); j++)
+
+        {
+
+            a[i, j] = random.Next(0, 40);
+
+        }
+
+    }
+
+}
+
+void Print(int[,] a)
+
+{
+
+    for (int i = 0; i < a.GetLength(0); i++)
+
+    {
+
+        for (int j = 0; j < a.GetLength(1); j++)
+
+        {
+
+            Console.Write($"{a[i, j],4}");
+
+        }
+
+        Console.WriteLine();
+
+    }
+
+}
+
+
+void ElementSpecific(int[,] a, int m)
+{
+   
+    Console.WriteLine(" Введите число k: ");
+    int k = Convert.ToInt32(Console.ReadLine());
+
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+
+            if (a[i, j] % m == k)
+            {
+                Console.Write($"\n элемент:{a[i, j]}");
+
+            }
+            
+        }
+
+    }
+
+
+
+
+}
+
+
+int m = 4;
+int n = 3;
+int[,] a = new int[n, m];
+SetArray2D(a);
+Print(a);
+Console.WriteLine();
+ElementSpecific(a,m);
